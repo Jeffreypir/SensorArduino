2bb15e14bf0567d8e752c96e3a6cd1ea267366ef
Jeffreypir <50076183+Jeffreypir@users.noreply.github.com>
2025-04-30 09:06:27 -0300

    Update estatistica.py
    Cabecalho

--- estatistica.py
+++ estatistica.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 #==================== PROGRAM ==============================
-# Program: <+name+>
-# Date of Create: <+date+>
-# Update in: <+update+>
+# Program: estatistica
+# Date of Create: 29/04/2025
+# Update in: 29/04/2025
 # Author:Jefferson Bezerra dos Santos
-# Description: <+description+>
+# Description: Gerar Estátisticas Percentuais
 #===========================================================
 
 import pandas as pd
